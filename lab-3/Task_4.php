17d5b3a4830ea460585ea3a9aaa259aa95288ca0
Nowshin Fariha <158089893+NowshinFariha@users.noreply.github.com>
2024-11-11 16:54:32 +0600

    Task_4.php

--- lab-3/Task_4.php
+++ lab-3/Task_4.php
@@ -1,1 +1,19 @@
+<?php
+$num1 =10;
+$num2 =25;
+$num3 =15;
 
+if($num1 >= $num2 && $num1 >= $num3)
+{
+    echo "the largest number is :".$num1;
+}
+elseif($num2 >= $num1 && $num2 >= $num3)
+{
+    echo"the largest number is : ".$num2;
+}
+else
+{
+    echo"the largest number is : ".$num3;
+}
+
+?>
