d271f75e19f367bdb765f47bc4896f434578bf34
Nowshin Fariha <158089893+NowshinFariha@users.noreply.github.com>
2024-11-11 16:53:11 +0600

    Task_1.php

--- lab-3/Task_1.php
+++ lab-3/Task_1.php
@@ -1,1 +1,23 @@
+<?php
+
+ function area($length,$width)
+ {
+     return $length * $width;
+ }
+
+ function perimeter($length,$width)
+ {
+     return 2 * ($length + $width);
+ }
+ $length =5;
+ $width =10;
+
+ $a= area($length,$width);
+ $b= perimeter($length,$width);
+
+ echo "the area of the rectangle is:".$a ."<br>";
+ echo "the perimeter of the rectangle is:".$b ."<br>";
+
+?>
+
 
