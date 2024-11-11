53fd8af4fc62e9afbb2310218712b9c7aee606a9
Nowshin Fariha <158089893+NowshinFariha@users.noreply.github.com>
2024-11-11 16:53:46 +0600

    Task_2.php

--- lab-3/Task_2.php
+++ lab-3/Task_2.php
@@ -1,1 +1,15 @@
+<?php
+ function vat($amount,$vatrate =15)
+ {
+     $v = ($amount * $vatrate)/100;
+     return $v;
+ }
+ $amount =100;
+ 
+ $v= vat($amount);
+
+ echo "the vat on $:".$amount."is $".$v;
+
+?>
+
 
